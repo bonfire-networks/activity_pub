2ad6f9bcd20d3a0dd5432f417d2e127382c74743
Karen Konou <konoukaren@gmail.com>
2020-07-21 12:43:48 +0200

    Fix coveralls

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule ActivityPub.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 

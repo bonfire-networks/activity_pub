68a4cd59d3b1503196b258a94064f4049e3b77cd
Karen Konou <konoukaren@gmail.com>
2020-07-21 12:27:00 +0200

    Tweak router macro

--- lib/activity_pub_web/router.ex
+++ lib/activity_pub_web/router.ex
@@ -1,5 +1,7 @@
 defmodule ActivityPubWeb.Router do
   defmacro __using__(_) do
+    ap_base_path = System.get_env("AP_BASE_PATH", "/pub")
+
     quote do
       pipeline :well_known do
         plug(:accepts, ["json", "jrd+json"])
@@ -21,9 +23,7 @@ defmodule ActivityPubWeb.Router do
         get "/nodeinfo", NodeinfoController, :schemas
       end
 
-      ap_base_path = System.get_env("AP_BASE_PATH", "/pub")
-
-      scope ap_base_path, ActivityPubWeb do
+      scope unquote(ap_base_path), ActivityPubWeb do
         pipe_through(:activity_pub)
 
         get "/objects/:uuid", ActivityPubController, :object
@@ -33,7 +33,7 @@ defmodule ActivityPubWeb.Router do
         get "/actors/:username/outbox", ActivityPubController, :noop
       end
 
-      scope ap_base_path, ActivityPubWeb do
+      scope unquote(ap_base_path), ActivityPubWeb do
         pipe_through(:signed_activity_pub)
 
         post "/actors/:username/inbox", ActivityPubController, :inbox

6092643df080b0ebeb63b6d3e8acf463f54aee16
Karen Konou <konoukaren@gmail.com>
2020-08-05 11:07:45 +0200

    Move instance migration to migrations.ex

--- lib/activity_pub/migrations.ex
+++ lib/activity_pub/migrations.ex
@@ -15,11 +15,25 @@ defmodule ActivityPub.Migrations do
 
     create unique_index(:ap_object, ["(data->>'id')"])
     create unique_index(:ap_object, [:pointer_id])
+
+    create table("ap_instance", primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :host, :string
+      add :unreachable_since, :naive_datetime_usec
+
+      timestamps()
+    end
+
+    create unique_index("ap_instance", [:host])
+    create index("ap_instance", [:unreachable_since])
   end
 
   def down do
     drop table("ap_object")
     drop index(:ap_object, ["(data->>'id')"])
     drop index(:ap_object, [:pointer_id])
+    drop table("ap_instance")
+    drop index("ap_instance", [:host])
+    drop index("ap_instance", [:unreachable_since])
   end
 end

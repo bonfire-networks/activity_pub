76e007828079b74e8433b36196a07d6e0f1fd4db
Mayel <git@mayel.space>
2020-10-29 16:53:54 +0100

    support any object type

--- lib/activity_pub/utils.ex
+++ lib/activity_pub/utils.ex
@@ -370,7 +370,9 @@ defmodule ActivityPub.Utils do
   """
   def insert_full_object(map, local \\ false, pointer \\ nil)
   def insert_full_object(%{"object" => %{"type" => type} = object_data} = map, local, pointer)
-      when is_map(object_data) and type in @supported_object_types do
+      when is_map(object_data)
+      # and type in @supported_object_types
+      do
     with nil <- Object.normalize(object_data, false),
          {:ok, data} <- prepare_data(object_data, local, pointer),
          {:ok, object} <- Object.insert(data) do

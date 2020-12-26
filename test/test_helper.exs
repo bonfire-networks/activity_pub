7a003ca4a88db5b64d84d4e9c6649b83898f6d21
Mayel <git@mayel.space>
2020-12-26 13:44:49 +0100

    initialise TestRepo for tests to run embedded in app

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,14 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 ExUnit.start()
+
+{:ok, _} = Ecto.Adapters.Postgres.ensure_all_started(ActivityPub.TestRepo, :temporary)
+
+# This cleans up the test database and loads the schema
+Mix.Task.run("ecto.create")
+Mix.Task.run("ecto.migrate")
+# Mix.Task.run("ecto.load")
+
+# Start a process ONLY for our test run.
+{:ok, _pid} = ActivityPub.TestRepo.start_link()
+
 Ecto.Adapters.SQL.Sandbox.mode(ActivityPub.TestRepo, :manual)

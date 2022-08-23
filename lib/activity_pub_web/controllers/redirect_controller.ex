9889b7e73b7290608933ceb9eccf348fd1f1c9de
Mayel de Borniol <git@mayel.space>
2022-08-23 15:27:45 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/441

--- lib/activity_pub_web/controllers/redirect_controller.ex
+++ lib/activity_pub_web/controllers/redirect_controller.ex
@@ -39,36 +39,36 @@ defmodule ActivityPubWeb.RedirectController do
     end
   end
 
-  # incoming remote follow
+  # incoming remote interaction
   def remote_interaction(conn, %{"acct" => username_or_uri}) do
-    with {:ok, actor} <- ActivityPub.Actor.get_or_fetch(username_or_uri),
-    url when is_binary(url) <- Adapter.get_redirect_url(actor) do
+    with {:ok, actor_or_object} <- ActivityPub.Actor.get_or_fetch(username_or_uri),
+    url when is_binary(url) <- Adapter.get_redirect_url(actor_or_object) do
       case url<>"?remote_interaction=follow" do
         "http"<>_ = url ->
           conn
-          |> put_flash(:info, "Press the follow button again to confirm that you want to follow this remote user.")
+          |> put_flash(:info, "Press the button again to confirm your action on this remote user or object.")
           |> redirect(external: url)
         url ->
           conn
-          |> put_flash(:info, "Press the follow button again to confirm that you want to follow this remote user.")
+          |> put_flash(:info, "Press the button again to confirm your action on this remote user or object.")
           |> redirect(to: url)
       end
     else _ ->
         conn
-        |> send_resp(404, "Actor not found")
+        |> send_resp(404, "Remote actor or object not found")
         |> halt
     end
   end
 
-  # outgoing remote follow
+  # outgoing remote interaction
   def remote_interaction(conn, %{"outgoing" => outgoing} = params) do
     me = Map.get(params, "me") || Map.get(outgoing, "me")
-    user_to_follow = Map.get(params, "follow") || Map.get(outgoing, "follow")
+    object = Map.get(params, "object") || Map.get(outgoing, "object") || Map.get(outgoing, "follow")
 
     with {:ok, fingered} <- ActivityPub.WebFinger.finger(me) |> debug("fingered"),
     %{"subscribe_address" => subscribe_address} when is_binary(subscribe_address) <- fingered,
     true <- String.contains?(subscribe_address, "{uri}"),
-    url <- String.replace(subscribe_address, "{uri}", user_to_follow) do
+    url <- String.replace(subscribe_address, "{uri}", object) do
       case url do
         "http"<>_ = url ->
           conn

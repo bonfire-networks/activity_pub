f6f78bd393087be2c6b33995b9e98b06299d2514
Mayel de Borniol <git@mayel.space>
2023-11-28 10:17:55 +0000

    digest

--- lib/web/plugs/digest_plug.ex
+++ lib/web/plugs/digest_plug.ex
@@ -3,25 +3,30 @@
 
 defmodule ActivityPub.Web.Plugs.DigestPlug do
   alias Plug.Conn
-  require Logger
+  import Untangle
 
   def read_body(conn, opts) do
-    digest_algorithm =
-      with [digest_header] <- Conn.get_req_header(conn, "digest") do
+    {:ok, body, conn} = Conn.read_body(conn, opts)
+
+    with [digest_header] <- Conn.get_req_header(conn, "digest") do
+      digest_algorithm =
         digest_header
         |> String.split("=", parts: 2)
         |> List.first()
-      else
-        _ -> "SHA-256"
+
+      unless String.downcase(digest_algorithm) == "sha-256" do
+        raise ArgumentError,
+          message: "invalid value for digest algorithm, got: #{digest_algorithm}"
       end
 
-    unless String.downcase(digest_algorithm) == "sha-256" do
-      raise ArgumentError,
-        message: "invalid value for digest algorithm, got: #{digest_algorithm}"
-    end
+      encoded_digest = :crypto.hash(:sha256, body) |> Base.encode64()
+      debug(encoded_digest, "encoded_digest")
 
-    {:ok, body, conn} = Conn.read_body(conn, opts)
-    encoded_digest = :crypto.hash(:sha256, body) |> Base.encode64()
-    {:ok, body, Conn.assign(conn, :digest, "#{digest_algorithm}=#{encoded_digest}")}
+      {:ok, body, Conn.assign(conn, :digest, "#{digest_algorithm}=#{encoded_digest}")}
+    else
+      _ ->
+        debug("no digest header so we skip computing the hash")
+        {:ok, body, conn}
+    end
   end
 end

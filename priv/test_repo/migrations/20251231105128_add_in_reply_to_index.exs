9db06a8a053dcbc4d80de23e7838f35422b3c5a6
ivan <ivanminutillo@outlook.it>
2026-01-09 09:58:22 +0100

    migrations

--- priv/test_repo/migrations/20251231105128_add_in_reply_to_index.exs
+++ priv/test_repo/migrations/20251231105128_add_in_reply_to_index.exs
@@ -2,6 +2,9 @@ defmodule ActivityPub.Repo.Migrations.AddInReplyToIndex do
   @moduledoc false
   use Ecto.Migration
 
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
   def up do
     ActivityPub.Migrations.add_object_in_reply_to_index()
   end
--- priv/test_repo/migrations/20260103105128_add_url_md5_index.exs
+++ priv/test_repo/migrations/20260103105128_add_url_md5_index.exs
@@ -2,6 +2,9 @@ defmodule ActivityPub.Repo.Migrations.AddURLMD5Index do
   @moduledoc false
   use Ecto.Migration
 
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
   def up do
     ActivityPub.Migrations.add_object_url_index()
   end
